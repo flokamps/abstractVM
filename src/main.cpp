6bdec32f2c972d56d0971fee75c8d1729e097a6c
Florian Kamps <florian.kamps@airbus.com>
2022-07-08 15:08:25 +0200

    coucou

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // Created by Florian Kamps on 07/07/2022.
 //
 
+#include "../inc/Parser.hpp"
 #include "../inc/Factory.hpp"
 
 int main(int argc, char **argv)
@@ -9,5 +10,7 @@ int main(int argc, char **argv)
     IOperand *int8 = Factory::createOperand(eOperandType::Float, "2.0");
     IOperand *int8_2 = Factory::createOperand(eOperandType::Double, "3.0");
     IOperand *sum = *int8 / *int8_2;
+    Parser parser;
+    parser.parse();
     return 0;
 }
