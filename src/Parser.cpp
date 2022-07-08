74d87af3205dad080062e0b23ac8d66b33d520f6
Julien <jdeviller18@gmail.com>
2022-07-08 14:46:16 +0200

    parser maj

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -15,19 +15,21 @@ void Parser::parsefrmfile()
 {
     std::ifstream asmFile(_file_path);
     std::string delimiter = " ";
+    std::string line;
 
-    while (asmFile) {
-        std::string line;
+    while (asmFile && line != "exit") {
         eOperandType t;
 
         getline(asmFile, line);
+        if (line == "exit")
+            continue;
         if (line.find(" ") != std::string::npos) {
             command = line.substr(0, line.find(delimiter));
             line.erase(0, line.find(delimiter) + delimiter.length());
-            t = _type[line.substr(0, line.find(delimiter))];
-            line.erase(0, line.find(delimiter) + delimiter.length());
+            t = _type[line.substr(0, line.find("("))];
+            line.erase(0, line.find("(") + delimiter.length());
             value = line.substr(0, line.find(delimiter));
-            value.erase(0, 1); value.pop_back();
+            value.pop_back();
             cmd.push_back({command, t, value});
         }
         else if (!line.empty()) {
@@ -46,22 +48,30 @@ void Parser::parse()
     std::string line;
     std::string delimiter = " ";
     eOperandType t;
+    int exit = 0;
 
-    while (line != "exit") {
+    while (line != ";;" && exit == 0) {
         getline(std::cin, line);
+        if (line == "exit") {
+            exit = 1;
+            continue;
+        }
         if (line.find(" ") != std::string::npos) {
             command = line.substr(0, line.find(delimiter));
             line.erase(0, line.find(delimiter) + delimiter.length());
-            t = _type[line.substr(0, line.find(delimiter))];
-            line.erase(0, line.find(delimiter) + delimiter.length());
+            t = _type[line.substr(0, line.find("("))];
+            line.erase(0, line.find("(") + delimiter.length());
             value = line.substr(0, line.find(delimiter));
-            value.erase(0, 1); value.pop_back();
+            value.pop_back();
             cmd.push_back({command, t, value});
         }
         else if (!line.empty() && line != "exit") {
             command = line;
             cmd.push_back({command, _type["NONE"], ""});
         }
+    }
+    while (line != ";;") {
+        getline(std::cin, line);
     }
     for(auto& tuple: cmd) {
         std::cout << std::get<0>(tuple) << " " << std::get<1>(tuple) << " " << std::get<2>(tuple) << std::endl;   
