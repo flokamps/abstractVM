4f631be210fb911a1c677af266d74b5fd755a62a
Julien <jdeviller18@gmail.com>
2022-07-09 10:33:11 +0200

    fixing parser.hpp + main

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 #include "../inc/VirtualMachine.hpp"
 
 int main(int ac, char **av) {
-    Parser parser(av[1]);
-    ac < 2 ? parser.parse() : parser.parsefrmfile();
+    try {
+        Parser parser(av[1]);
+        ac < 2 ? parser.parse() : parser.parsefrmfile();
+    } catch (ParserException &e) {
+        std::cerr << "ParserException: " << e.what() << std::endl;
+    }
     try {
         VirtualMachine vm;
         vm.run(parser.getInstructions());
