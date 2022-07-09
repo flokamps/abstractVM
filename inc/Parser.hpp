32db1fc19c6bfad28a0ed93c123e96e36089337d
Julien <jdeviller18@gmail.com>
2022-07-09 10:12:01 +0200

    adding handling error without exceptions working

--- inc/Parser.hpp
+++ inc/Parser.hpp
@@ -6,7 +6,9 @@
 */
 
 #include "IOperand.hpp"
+#include "Exception.hpp"
 
+#include <stdexcept>
 #include <fstream>
 #include <string>
 #include <iostream>
@@ -22,11 +24,13 @@ class Parser {
         Parser(char *file_path = nullptr);
         void parsefrmfile();
         void parse();
+        void errorHandling(eOperandType t);
+        bool is_number(const std::string& s);
         std::vector<std::tuple<std::string, eOperandType, std::string>> getInstructions();
 
     private:
         char *_file_path;
-        std::vector<std::tuple<std::string, eOperandType, std::string>> cmd;
+        std::vector<std::tuple<std::string, eOperandType, std::string>> instructions;
         std::string command;
         eOperandType type;
         std::string value;
@@ -34,4 +38,6 @@ class Parser {
             {"int8", Int8}, {"int16", Int16}, {"int32", Int32}, {"float", Float}, 
             {"double", Double}, {"bigdecimal", BigDecimal}, {"NONE", Null}
         };
+        std::vector<std::string> cmd {"pop", "dump", "clear", "dup", "swap", "add", "sub", "mul", "div", "mod", "print", "exit"};
+        std::vector<std::string> cmdv {"push", "assert", "load", "store"};
 };
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "../inc/Parser.hpp"
+#include <algorithm>
 
 Parser::Parser(char *file_path) {
     this->_file_path = file_path;
@@ -17,6 +18,9 @@ void Parser::parsefrmfile()
     std::string delimiter = " ";
     std::string line;
 
+    if (asmFile.is_open() == false) {
+        //throw error (file is not accessible)
+    }
     while (asmFile && line != "exit") {
         eOperandType t;
 
@@ -28,11 +32,11 @@ void Parser::parsefrmfile()
             line.erase(0, line.find("(") + delimiter.length());
             value = line.substr(0, line.find(delimiter));
             value.pop_back();
-            cmd.push_back({command, t, value});
+            instructions.push_back({command, t, value});
         }
         else if (!line.empty()) {
             command = line;
-            cmd.push_back({command, _type["NONE"], ""});
+            instructions.push_back({command, _type["NONE"], ""});
         }
     }
     asmFile.close();
@@ -57,18 +61,64 @@ void Parser::parse()
             line.erase(0, line.find("(") + delimiter.length());
             value = line.substr(0, line.find(delimiter));
             value.pop_back();
-            cmd.push_back({command, t, value});
+            errorHandling(t);
+            instructions.push_back({command, t, value});
         }
         else if (!line.empty()) {
             command = line;
-            cmd.push_back({command, _type["NONE"], ""});
+            errorHandling(t);
+            instructions.push_back({command, _type["NONE"], ""});
         }
     }
     while (line != ";;") {
         getline(std::cin, line);
     }
+    for(auto& tuple: instructions) {
+        std::cout << std::get<0>(tuple) << " " << std::get<1>(tuple) << " " << std::get<2>(tuple) << std::endl;   
+    }
 }
 
 std::vector<std::tuple<std::string, eOperandType, std::string>> Parser::getInstructions() {
-    return (cmd);
+    return (instructions);
+}
+
+bool Parser::is_number(const std::string& s)
+{
+    return !s.empty() && std::find_if(s.begin(), 
+        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
+
+void Parser::errorHandling(eOperandType t)
+{
+    int check = 0;
+
+    for (unsigned int i = 0; i < cmd.size(); i++) {
+        if (command == cmd[i])
+            check = 1;
+    }
+    if (check == 0) {
+        for (unsigned int i = 0; i < cmdv.size(); i++) {
+            if (command == cmdv[i])
+                check = 2;
+        }
+    }
+    switch (check) {
+        case 0:
+            throw ParserException("Error: bad command");
+            break;
+        case 1: {
+            if (t == 0)
+                throw ParserException("Error: bad type");
+            if (value != "")
+                throw ParserException("Error: bad value");
+            break;
+        } 
+        case 2: {
+            if (t == 0)
+                throw ParserException("Error: bad type");
+            if (!is_number(value))
+                throw ParserException("Error: bad value");
+            break;
+        }
+    }
 }
