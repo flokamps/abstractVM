e9b9bcf431ecf9a8dafb67a5fbc09d4bec4f03d8
Florian Kamps <florian.kamps@airbus.com>
2022-07-11 17:23:04 +0200

    fix trailing 0

--- src/VirtualMachine.cpp
+++ src/VirtualMachine.cpp
@@ -75,7 +75,7 @@ void VirtualMachine::removeTrailing0(std::stringstream &ss)
 
     if (str.find('.') != std::string::npos) {
            str = str.substr(0, str.find_last_not_of('0') + 1);
-           if (str.find('.') == str.size() + 1) {
+           if (str.find('.') == str.size() - 1) {
                str = str.substr(0, str.size() - 1);
            }
     }
